0d22479857541bc57c59adf2e6b143e74ca66aad
Paul Cochrane <paul@liekut.de>
2014-07-12 23:12:50 +0200

    Implementing extracting, plotting and saving minima data

--- arctic_ice_extent.pl
+++ arctic_ice_extent.pl
@@ -97,6 +97,63 @@ sub plot_sea_ice_extent {
     $chart->plot2d($data_set);
 }
 
+sub extract_minima {
+    my ($dates_ref, $extents_data_ref) = @_;
+    my @dates = @$dates_ref;
+    my %extents_data = %$extents_data_ref;
+
+    my %minima;
+    for my $date ( @dates ) {
+        $date =~ m/^(\d{4})/;
+        my $year = $1;
+        my $extent = $extents_data{$date};
+        $minima{$year} = 100 if not defined $minima{$year};
+        $minima{$year} = $extent if $extent < $minima{$year};
+    }
+
+    return \%minima;
+}
+
+sub save_minima_data {
+    my ($years_ref, $minima_ref) = @_;
+    my @years = @$years_ref;
+    my %minima = %$minima_ref;
+
+    open my $out_fh, ">", "minima.dat";
+    for my $year ( @years ) {
+        print $out_fh $year, " ", $minima{$year}, "\n";
+    }
+    close $out_fh;
+}
+
+sub plot_extent_minima {
+    my ($years_ref, $minima_ref) = @_;
+
+    my @years = @$years_ref;
+    my @minima = @$minima_ref;
+
+    my $chart = Chart::Gnuplot->new(
+        output => "minima.eps",
+        title => "Arctic sea ice extent minima over time",
+        xlabel => "Year",
+        ylabel => "Extent (10^6 km)",
+        timeaxis => 'x',
+        xtics => {
+            labelfmt => "%Y",
+            rotate => -90,
+        },
+    );
+
+    my $data_set = Chart::Gnuplot::DataSet->new(
+        xdata => \@years,
+        ydata => \@minima,
+        style => "lines",
+        timefmt => "%Y",
+    );
+
+    $chart->plot2d($data_set);
+}
+
 sub extract_ice_extent_data {
     my $extents_data_ref = get_extents_data();
     my %extents_data = %$extents_data_ref;
@@ -106,6 +163,15 @@ sub extract_ice_extent_data {
 
     my @extents = map { $extents_data{$_} } @dates;
     plot_sea_ice_extent(\@dates, \@extents);
+
+    my $minima_ref = extract_minima(\@dates, \%extents_data);
+    my %minima = %$minima_ref;
+
+    my @years = sort keys %minima;
+    save_minima_data(\@years, \%minima);
+
+    my @minima = map { $minima{$_} } @years;
+    plot_extent_minima(\@years, \@minima);
 }
 
 extract_ice_extent_data() unless caller();
