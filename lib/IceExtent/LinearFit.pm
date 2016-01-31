a7927ba9d16779edee5f0082b3027231d6c51039
Paul Cochrane <paul@liekut.de>
2016-01-31 18:10:11 +0100

    Implement linear fits of data
    It turned out that the `params` attribute/method wasn't necessary and one
    could just ask the fit object directly for the fit parameters (duh).

--- lib/IceExtent/LinearFit.pm
+++ lib/IceExtent/LinearFit.pm
@@ -1,7 +1,8 @@
 package IceExtent::LinearFit;
 
 use Moo;
-use Types::Standard qw(ArrayRef HashRef);
+use Types::Standard qw(ArrayRef Num);
+use Algorithm::CurveFit;
 
 has xdata => (
     is       => 'rw',
@@ -15,10 +16,47 @@ has ydata => (
     required => 1,
 );
 
-has params => (
+has a => (
     is => 'rw',
-    isa => HashRef,
+    isa => Num,
 );
+
+has b => (
+    is => 'rw',
+    isa => Num,
+);
+
+has R2 => (
+    is => 'rw',
+    isa => Num,
+);
+
+sub fit {
+    my $self = shift;
+
+    my $formula = 'b + a * x';
+    my $variable = 'x';
+
+    my @parameters = (
+        # Name    Guess   Accuracy
+        ['a',     1,     0.00001],
+        ['b',     1,     0.00001],
+    );
+    my $max_iter = 1000; # maximum iterations
+
+    my $square_residual = Algorithm::CurveFit->curve_fit(
+        formula            => $formula,
+        params             => \@parameters,
+        variable           => $variable,
+        xdata              => $self->xdata,
+        ydata              => $self->ydata,
+        maximum_iterations => $max_iter,
+    );
+
+    $self->a($parameters[0][1]);
+    $self->b($parameters[1][1]);
+    $self->R2($square_residual);
+}
 
 1;
 
--- t/03-ice-extent-linear-fit.t
+++ t/03-ice-extent-linear-fit.t
@@ -2,15 +2,34 @@ use strict;
 use warnings;
 
 use lib qw(lib ../lib);
-use Test::More tests => 1;
+use Test::More tests => 2;
+use Test::Approx;
 
 subtest "basic object setup is correct" => sub {
     plan tests => 3;
 
     require_ok('IceExtent::LinearFit');
-    can_ok( 'IceExtent::LinearFit', qw(params) );
+    can_ok( 'IceExtent::LinearFit', qw(a b R2) );
     isa_ok( IceExtent::LinearFit->new( xdata => [], ydata => [] ),
         'IceExtent::LinearFit' );
+};
+
+subtest "fitting a linear data set gives expected fit" => sub {
+    plan tests => 3;
+
+    my @xdata = ( 1, 2, 3, 4, 5, 6, 7, 8, 9 );
+    my @ydata = map { 2 * $_ } @xdata;
+    my $linear_fit =
+      IceExtent::LinearFit->new( xdata => \@xdata, ydata => \@ydata );
+    $linear_fit->fit;
+
+    my $tolerance = 1e-14;
+    is_approx_num $linear_fit->a, 2,
+      "Fit gives correct 'a' parameter value", $tolerance;
+    is_approx_num $linear_fit->b, 0,
+      "Fit gives correct 'b' parameter value", $tolerance;
+    is_approx_num $linear_fit->R2, 0,
+      "Fit gives correct 'R^2' parameter value", $tolerance;
 };
 
 # vim: expandtab shiftwidth=4 softtabstop=4
