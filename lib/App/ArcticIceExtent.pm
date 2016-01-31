e10649b8242668ab0d9efc76a6c29187478f9a9c
Paul Cochrane <paul@liekut.de>
2016-01-31 13:04:18 +0100

    Add basic plotting code to main application
    This helps to check that all the code is working by integrating the parts.

--- lib/App/ArcticIceExtent.pm
+++ lib/App/ArcticIceExtent.pm
@@ -1,11 +1,24 @@
 package App::ArcticIceExtent;
 
 use Moo;
+use IceExtent::Data;
+use IceExtent::Plot;
 
 our $VERSION = 0.001;
 
 sub run {
-    die "not implemented";
+    my $data = IceExtent::Data->new;
+    $data->fetch;
+    $data->load;
+
+    my $chart = IceExtent::Plot->new(
+        data => [ $data->dates, $data->extents ],
+        title => "Extents",
+        filename => "blah.png",
+        time_format => '%Y-%m-%d',
+    );
+    $chart->plot;
+
 }
 
 1;
